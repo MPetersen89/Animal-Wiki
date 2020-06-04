0101bc45e8c2754d9364002732d89b67464f5642
Michael <michael_petersen_email@yahoo.com>
2020-06-04 11:49:04 -0700

    create animal wiki page with basic css and js styling/functionality.
    Co-authored by: Kyle Hubbard <kyle.james.hubbard@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("#unicorn-img").click(function() {
+    $("#unicorn").slideToggle();
+  });
+
+  $("#liger-img").click(function() {
+    $("#liger").slideToggle();
+  });
+
+  $("#honeyBadger-img").click(function() {
+    $("#honeyBadger").slideToggle();
+  });
+  
+  event.preventDefault();
+})
